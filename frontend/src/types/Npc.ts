bc4b9eec29e5f95bc5fa3ab7524ad154b3ab7b0c
RicardoRuggiero <pollar123@gmail.com>
2026-06-16 18:13:07 -0300

    primeiro commit do front

---
+++ frontend/src/types/Npc.ts
@@ -0,0 +1,10 @@
+
+export interface Npc {
+  id: number;
+  name: string | null;
+  mapname: string;
+  x: number;
+  y: number;
+  job: number;
+  type: string;
+}
---
+++ frontend/src/types/Soldby.ts
@@ -0,0 +1,10 @@
+
+import type { Item } from "./Item";
+import type { Npc } from "./Npc";
+
+export interface Soldby {
+  id: number;
+  price: number;
+  item: Item;
+  npc: Npc;
+}
